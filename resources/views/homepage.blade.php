e720cd2a3bacce5c210fc393cf629a13357d2d7a
blour <bloureiro2004@gmail.com>
2024-06-06 13:36:59 +0100

    pila

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CineMagic - O centro do cinema!</title>
+    <h1>aoifaeefhou</h1>
 </head>
 <body>
     
