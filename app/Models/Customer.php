c89086c77366fa783d10bd5f8451c10d7d2f0dd9
Rafael Rosa <rafaelrosa1004@hotmail.com>
2024-06-13 14:44:27 +0100

    Minor spelling mistake

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User;
 
 class Customer extends Model
 {
