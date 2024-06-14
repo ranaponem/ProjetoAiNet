af09e3bda18da8f9e261e6415fd0f9ee1705c697
Rafael Rosa <rafaelrosa1004@hotmail.com>
2024-06-14 13:13:49 +0100

    Theater Policy

---
+++ app/Policies/TheaterPolicy.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Theater;
+use App\Models\User;
+
+class TheaterPolicy
+{
+    public function before(User $user): ?bool
+    {
+        if ($user->type === 'A') {
+            return true;
+        }
+        return null;
+    }
+
+    public function viewAny(User $user): bool
+    {
+        return $user->type === 'A';
+    }
+
+
+    public function view(User $user, Theater $theater): bool
+    {
+        return $user->type === 'A';
+    }
+
+    public function create(User $user): bool
+    {
+        return $user->type === 'A';
+    }
+
+    public function update(User $user, Theater $theater): bool
+    {
+        return $user->type === 'A';
+    }
+
+    public function delete(User $user, Theater $theater): bool
+    {
+        return $user->type === 'A';
+    }
+}
