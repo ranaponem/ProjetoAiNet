9f27e8a2cc593b12de2d5ad485b758f1b38faa57
Rafael Rosa <rafaelrosa1004@hotmail.com>
2024-06-14 16:04:02 +0100

    Fucking

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,92 +10,15 @@
     <!-- Scripts AND CSS Fields -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<div class="min-h-screen bg-gray-100 dark:bg-gray-800">
-
-        <!-- Navigation Menu -->
-        <nav class="bg-white dark:bg-gray-900 border-b border-gray-100 dark:border-gray-800">
-            <!-- Navigation Menu Full Container -->
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <!-- Logo + Menu Items + Hamburger -->
-                <div class="relative flex flex-col sm:flex-row px-6 sm:px-0 grow justify-between">
-                    <!-- Logo -->
-                    <div class="shrink-0 -ms-4">
-                        <a href="{{ route('home')}}">
-                            <div class="h-16 w-16 bg-cover bg-[url('../img/templogo.svg')] dark:bg-[url('../img/templogo.svg')]"></div>
-                        </a>
-                    </div>
-
-                    <!-- Menu Items -->
-                    <div id="menu-container" class="grow flex flex-col sm:flex-row items-stretch
-                    invisible h-0 sm:visible sm:h-auto">
-
-                        <div class="grow"></div>
-
-                        <!-- Menu Item: Cart -->
-                        @if (session('cart'))
-                            @can('use-cart')
-                            <x-menus.cart
-                                :href="route('cart.show')"
-                                selectable="1"
-                                selected="{{ Route::currentRouteName() == 'cart.show'}}"
-                                :total="session('cart')->count()"/>
-                            @endcan
-                        @endif
-
-                        @auth
-                        <x-menus.submenu
-                            selectable="0"
-                            uniqueName="submenu_user"
-                            >
-                            <x-slot:content>
-                                <div class="pe-1">
-                                    <img src="{{Auth::user()->getImageUrlAttribute()}}" class="w-11 h-11 min-w-11 min-h-11 rounded-full">
-                                </div>
-                                <div class="ps-1 sm:max-w-[calc(100vw-39rem)] md:max-w-[calc(100vw-41rem)] lg:max-w-[calc(100vw-46rem)] xl:max-w-[34rem] truncate">
-                                    {{ Auth::user()->name }}
-                                </div>
-                            </x-slot>
-                            @auth
-                            <hr>
-                            <x-menus.submenu-item
-                                content="Profile"
-                                selectable="0"
-                                href="{{ route('profile.edit')}} "/>
-                            @endauth
-                            <hr>
-                            <form id="form_to_logout_from_menu" method="POST" action="{{ route('logout') }}" class="hidden">
-                                @csrf
-                            </form>
-                            <x-menus.submenu-item
-                                content="Log Out"
-                                selectable="0"
-                                form="form_to_logout_from_menu"/>
-                        </x-menus.submenu>
-                        @else
-                        <!-- Menu Item: Login -->
-                        <x-menus.menu-item
-                            content="Login"
-                            selectable="1"
-                            href="{{ route('login') }}"
-                            selected="{{ Route::currentRouteName() == 'login'}}"
-                            />
-                        @endauth
-                    </div>
-                </div>
-            </div>
-        </nav>
-
-        <!-- Page Heading -->
-        <header class="bg-white dark:bg-gray-800 shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                
-                <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                    @yield('header-title')
-                </h2>
-            </div>
-        </header>
-
+<body>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            @yield('header-title')
+        </h2>
+    </x-slot>
         <main>
+
             <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
                 @if (session('alert-msg'))
                     <x-alert type="{{ session('alert-type') ?? 'info' }}">
@@ -109,5 +32,6 @@
             </div>
         </main>
     </div>
+</x-app-layout>
 </body>
 </html>
--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -3,23 +3,57 @@
 @section('header-title', $movie->title)
 
 @section('main')
+    <style>
+        .no-scrollbar {
+            -ms-overflow-style: none; /* IE and Edge */
+            scrollbar-width: none; /* Firefox */
+        }
+        .no-scrollbar::-webkit-scrollbar {
+            display: none; /* Hide scrollbar for Chrome, Safari, and Opera */
+        }
 
+    </style>
 <main>
-    <div class="h-full flex flex-wrap justify-center">
-        <div class="w-full h-full rounded shadow-lg bg-white dark:bg-gray-900 p-2 ">
-            <div class="h-1/6 p-4 flex">
-                <img class="h-1/6 w-auto inline-block mr-4 rounded" src="{{ $movie->getImageUrlAttribute() }}" alt="{{ $movie->poster_filename }}">
+    <div class="flex h-screen flex-wrap justify-center">
+        <div class="w-full h-screen rounded shadow-lg bg-white dark:bg-gray-900 p-2 ">
+            <div class="h-2/5 flex overflow-y-scroll no-scrollbar">
+                <img class="h-full w-auto inline-block mr-4 rounded" src="{{ $movie->getImageUrlAttribute() }}" alt="{{ $movie->poster_filename }}">
                 <div class="inline-block flex flex-col justify-start">
                     
-                    <h1 class="font-bold text-3xl text-black dark:text-gray-50 mb-5">Synopsis:</h1>
-                    <p class="font-bold text-black dark:text-gray-50">{{ $movie->synopsis }}</p>
+                    <h1 class="font-bold text-3xl text-black dark:text-gray-50 mb-8">Synopsis:</h1>
+                    <p class="font-bold text-xl text-black dark:text-gray-50">{{ $movie->synopsis }}</p>
+                    <p class="font-bold mt-8 text-xl text-black dark:text-gray-50">Genre: {{ $movie->genre_code }}</p>
+                    <p class="font-bold text-xl text-black dark:text-gray-50">Year: {{ $movie->year }}</p>
 
                 </div> 
                 
+                
             </div>
-        </div>
+            <div class="mt-10 flex">
+                <h1 class="w-3/6 inline-block font-bold text-3xl text-black dark:text-gray-50 mb-5 ml-5">Trailer:</h1>
+                <h1 class="w-1/3 inline-block font-bold text-3xl text-black dark:text-gray-50 mb-5">Select a screening:</h1>
+            </div>
+            <div class=" h-2/5 flex mb-5">
+                <div class="w-1/2 mt-4 flex ml-5">
+                    <iframe width="90%" height="auto" src="{{ $movie->trailer_url }}" frameborder="0" allowfullscreen></iframe>
+                </div>
+                <div class="overflow-y-scroll no-scrollbar mt-4  justify-center">
+                    
+                        @foreach ($screenings as $screening)
+                        <a href="#">
+                            <div class="flex hover:bg-gray-800">
+                               <h1 class="inline-block text-center font-bold p-4 text-black dark:text-gray-50">Theater: {{$screening->theater->name}} </h1> 
+                               <h1 class="inline-block text-center font-bold p-4 text-black dark:text-gray-50">Date: {{$screening->date}} {{$screening->start_time}}</h1> 
+                            </div>
+                            
+                        </a>
+                        @endforeach
 
+                </div>
+        </div>
     </div>
+
+
     
 </main>
 
