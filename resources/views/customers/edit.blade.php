29dd8a1f0aebafceaa30f70a2512dc730867ed53
MigueXYZ <miguelito.gomes.silva@gmail.com>
2024-06-15 16:32:47 +0100

    AAAAAAAAAAAAAAAAAAAAAA

--- resources/views/customers/edit.blade.php
+++ resources/views/customers/edit.blade.php
@@ -7,6 +7,8 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+
+
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     <section>
@@ -21,7 +23,8 @@
                             @method('put')
 
                             <div>
-                                <x-input-label for="Payment type" :value="__('Payment type')" class="mb-6"/>
+
+                                <x-input-label for="payment_type" :value="__('Payment type')" class="mb-6"/>
                                 <input type="radio" id="visa" name="payment_type" value="VISA" @if ($customer->payment_type === 'VISA') checked @endif >
                                 <label for="visa" class="text-gray-300 ml-2">VISA</label><br>
                                 <input type="radio" id="mbway" name="payment_type" value="MBWAY" @if ($customer->payment_type === 'MBWAY') checked @endif>
@@ -29,19 +32,24 @@
                                 <input type="radio" id="paypal" name="payment_type" value="PAYPAL" @if ($customer->payment_type === 'PAYPAL') checked @endif>
                                 <label for="paypal" class="text-gray-300 ml-2">PAYPAL</label>
                                 <x-input-error class="mt-2" :messages="$errors->get('Payment_type')" />
+
+
+
                             </div>
 
                             <div>
-                                <x-input-label for="payment ref" :value="__('Payment Reference')" />
+                                <x-input-label for="payment_ref Reference" :value="__('Payment Reference')" />
                                 <x-text-input id="payment_ref" name="payment_ref" type="text" class="mt-1 block w-full" :value="old('payment_ref', $customer->payment_ref)" required  autocomplete="payment_ref" />
-                                <x-input-error class="mt-2" :messages="$errors->get('Payment_Reference')" />
+                                <x-input-error class="mt-2" :messages="$errors->get('payment_ref')" />
                             </div>
-
                             <div>
+
                                 <x-input-label for="nif" :value="__('NIF')" />
                                 <x-text-input id="nif" name="nif" type="text" class="mt-1 block w-full" :value="old('nif', $customer->nif)" required  autocomplete="nif" />
                                 <x-input-error class="mt-2" :messages="$errors->get('nif')" />
                             </div>
+
+
 
                             <div class="flex items-center gap-4">
                                 <x-primary-button>{{ __('Save') }}</x-primary-button>
@@ -59,8 +67,11 @@
                             </div>
                         </form>
                     </section>
+
                 </div>
             </div>
+
+
         </div>
     </div>
 </x-app-layout>
